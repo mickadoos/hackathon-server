9c5e2ad9de01d0214729899af650dec5430e8db6
Raquel Barrio <r.barrio.f@gmail.com>
2023-06-20 17:02:15 +0200

    itinerary routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
+const Itinerary = require("../models/Itinerary.model");
+const Location = require("../models/Location.model");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.get("/itinerary/:itineraryId/", (req, res, next)=>{
+  let {itineraryId} = req.params;
+  Itinerary.findById(itineraryId)
+  .populate("attraction locationList")
+  .then((itinerary)=>{
+    res.json(itinerary)
+  })
+  .catch((err)=>next(err))
+}) 
+
+router.post("/createItinerary", (req, res, next) => {
+  let { attraction, locationList } = req.body;
+  const newItinerary = { attraction, locationList };
+  Itinerary.create(newItinerary)
+  .then((itinerary)=>{
+    res.json(itinerary)
+  })
+  .catch((err)=>next(err))
+})
 
 module.exports = router;
