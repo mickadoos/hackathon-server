6d15834d2bd8f5759cf2b89a5b740355ce76f84d
Yabel <yabelrr@gmail.com>
2023-06-20 17:07:11 +0200

    Back route createLocation working and tested

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const Location = require('../models/Location.model')
+const Itinerary = require('../models/Itinerary.model')
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.post("/createLocation", (req, res, next) => {
+  const {name, picture, description, popularity, coordinates} = req.body
+  Location.create({name, picture, description, popularity, coordinates})
+  .then(response => {
+    res.json({message: 'Location succesfully created!', location: response})
+  })
+  .catch(err => {next(err)})
+
+});
+
 
 module.exports = router;
